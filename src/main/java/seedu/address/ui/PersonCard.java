45bf2ca335c924898668aaf961d6415abd00baa0
deborahlow97 <deborahlow97@gmail.com>
2018-03-12 13:42:57 +0800

    edit

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -50,7 +50,6 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(person.getPhone().value);
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
-        AddLevelOfFriendshipClass
         levelOfFriendship.setText(person.getLevelOfFriendship().value);
         initTags(person);
     }
