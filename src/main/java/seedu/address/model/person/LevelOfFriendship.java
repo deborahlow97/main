a3401442cd9b03c6c3d75743cbe452386bb11b66
deborahlow97 <deborahlow97@gmail.com>
2018-03-14 17:35:26 +0800

    Add changes and constraints

--- src/main/java/seedu/address/model/person/LevelOfFriendship.java
+++ src/main/java/seedu/address/model/person/LevelOfFriendship.java
@@ -9,15 +9,18 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class LevelOfFriendship {
 
-    public static final String MESSAGE_LEVEL_OF_FRIENDSHIP_CONSTRAINTS =
-            "Level of Friendship should only be a numerical value between 1 to 10";
 
+
+    public static final String MESSAGE_LEVEL_OF_FRIENDSHIP_CONSTRAINTS =
+            "Level of Friendship should only be a numerical integer value between 1 to 10";
+    public static final int MINIMUM_LEVEL_OF_FRIENDSHIP = 1;
+    public static final int MAXIMUM_LEVEL_OF_FRIENDSHIP = 10;
     public static final String LEVEL_OF_FRIENDSHIP_VALIDATION_REGEX = "[0-9]+";
+    private static int levelOfFriendshipInIntegerForm;
     public final String value;
 
-
     /**
-     * Constructs an {@code LevelOfFriendship}.
+     * * Constructs an {@code LevelOfFriendship}.
      *
      * @param levelOfFriendship A valid level of friendship number.
      */
@@ -32,7 +35,22 @@ public class LevelOfFriendship {
      * Returns if a given string is a valid person level of friendship.
      */
     public static boolean isValidLevelOfFriendship(String test) {
-        return test.matches(LEVEL_OF_FRIENDSHIP_VALIDATION_REGEX);
+
+        return test.matches(LEVEL_OF_FRIENDSHIP_VALIDATION_REGEX) && isAnIntegerWithinRange(test);
+    }
+
+    /**
+     * Returns if a given string is an integer and within range of level of friendship.
+     */
+    private static boolean isAnIntegerWithinRange(String test) {
+        levelOfFriendshipInIntegerForm = Integer.parseInt(test);
+        if (levelOfFriendshipInIntegerForm >= MINIMUM_LEVEL_OF_FRIENDSHIP
+                && levelOfFriendshipInIntegerForm <= MAXIMUM_LEVEL_OF_FRIENDSHIP) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     @Override
