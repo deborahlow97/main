abed8f46d37ad07a0bdbd346a21c56e78ad580d0
deborahlow97 <deborahlow97@gmail.com>
2018-03-21 14:50:18 +0800

    Edit tests

--- src/test/java/systemtests/AddCommandSystemTest.java
+++ src/test/java/systemtests/AddCommandSystemTest.java
@@ -33,7 +33,6 @@ import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_UNIT_NUMBER_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_UNIT_NUMBER_BOB;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CCA;
